942bceb82189995f26ef84b97ed5605a523c8195
Ranjan Bhagat <89478394+Ranjan-Bhagat@users.noreply.github.com>
2026-07-10 18:17:34 +0530

    feat: add ffmpeg microphone discovery

---
+++ lib/src/audio/ffmpeg_microphone_discovery.dart
@@ -0,0 +1,93 @@
+import 'dart:io';
+
+class MicrophoneDevice {
+  const MicrophoneDevice({required this.name, this.alternativeName});
+
+  final String name;
+  final String? alternativeName;
+}
+
+class DiscoveryProcessResult {
+  const DiscoveryProcessResult({required this.exitCode, required this.output});
+
+  final int exitCode;
+  final String output;
+}
+
+abstract interface class DiscoveryProcessRunner {
+  Future<DiscoveryProcessResult> run(String executable, List<String> arguments);
+}
+
+class FfmpegMicrophoneDiscovery {
+  const FfmpegMicrophoneDiscovery({
+    this.processRunner = const DartDiscoveryProcessRunner(),
+  });
+
+  final DiscoveryProcessRunner processRunner;
+
+  Future<List<MicrophoneDevice>> listMicrophones() async {
+    final result = await processRunner.run('ffmpeg', [
+      '-hide_banner',
+      '-list_devices',
+      'true',
+      '-f',
+      'dshow',
+      '-i',
+      'dummy',
+    ]);
+
+    return parseDirectShowDevices(result.output);
+  }
+
+  static List<MicrophoneDevice> parseDirectShowDevices(String output) {
+    final devices = <MicrophoneDevice>[];
+    String? pendingAudioDevice;
+
+    for (final line in output.split(RegExp(r'\r?\n'))) {
+      final deviceMatch = RegExp(r'"(.+)" \((audio|video)\)').firstMatch(line);
+      if (deviceMatch != null) {
+        if (deviceMatch.group(2) == 'audio') {
+          pendingAudioDevice = deviceMatch.group(1);
+        } else {
+          pendingAudioDevice = null;
+        }
+        continue;
+      }
+
+      final alternativeNameMatch = RegExp(
+        r'Alternative name "(.+)"',
+      ).firstMatch(line);
+      if (alternativeNameMatch != null && pendingAudioDevice != null) {
+        devices.add(
+          MicrophoneDevice(
+            name: pendingAudioDevice,
+            alternativeName: alternativeNameMatch.group(1),
+          ),
+        );
+        pendingAudioDevice = null;
+      }
+    }
+
+    if (pendingAudioDevice != null) {
+      devices.add(MicrophoneDevice(name: pendingAudioDevice));
+    }
+
+    return devices;
+  }
+}
+
+class DartDiscoveryProcessRunner implements DiscoveryProcessRunner {
+  const DartDiscoveryProcessRunner();
+
+  @override
+  Future<DiscoveryProcessResult> run(
+    String executable,
+    List<String> arguments,
+  ) async {
+    final result = await Process.run(executable, arguments);
+    return DiscoveryProcessResult(
+      exitCode: result.exitCode,
+      output: '${result.stdout}${result.stderr}',
+    );
+  }
+}
---
+++ test/ffmpeg_microphone_discovery_test.dart
@@ -0,0 +1,56 @@
+import 'package:dictation_flow/src/audio/ffmpeg_microphone_discovery.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('parses audio devices from ffmpeg dshow device output', () {
+    const output = r'''
+[in#0 @ 0000023546537a00] "Brio 100" (video)
+[in#0 @ 0000023546537a00]   Alternative name "@device_pnp_\\?\usb#vid_046d&pid_094c&mi_00#7&5aade7c&0&0000#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\global"
+[in#0 @ 0000023546537a00] "Microphone (Brio 100)" (audio)
+[in#0 @ 0000023546537a00]   Alternative name "@device_cm_{33D9A762-90C8-11D0-BD43-00A0C911CE86}\wave_{79BD100C-9ACE-43FE-9839-3D0C45F44B69}"
+[in#0 @ 0000023546537a00] "Microphone Array (Intel® Smart Sound Technology for Digital Microphones)" (audio)
+[in#0 @ 0000023546537a00]   Alternative name "@device_cm_{33D9A762-90C8-11D0-BD43-00A0C911CE86}\wave_{1906B999-D02F-4207-9CC7-0C0908C25531}"
+Error opening input file dummy.
+''';
+
+    final devices = FfmpegMicrophoneDiscovery.parseDirectShowDevices(output);
+
+    expect(devices.map((device) => device.name), [
+      'Microphone (Brio 100)',
+      'Microphone Array (Intel® Smart Sound Technology for Digital Microphones)',
+    ]);
+    expect(devices.first.alternativeName, contains('wave_'));
+  });
+
+  test('list returns parsed devices from the process runner output', () async {
+    final discovery = FfmpegMicrophoneDiscovery(
+      processRunner: FakeDiscoveryProcessRunner('''
+[in#0] "External Mic" (audio)
+[in#0]   Alternative name "alt-external"
+'''),
+    );
+
+    final devices = await discovery.listMicrophones();
+
+    expect(devices.single.name, 'External Mic');
+    expect(devices.single.alternativeName, 'alt-external');
+  });
+}
+
+class FakeDiscoveryProcessRunner implements DiscoveryProcessRunner {
+  FakeDiscoveryProcessRunner(this.output);
+
+  final String output;
+  String? executable;
+  List<String>? arguments;
+
+  @override
+  Future<DiscoveryProcessResult> run(
+    String executable,
+    List<String> arguments,
+  ) async {
+    this.executable = executable;
+    this.arguments = arguments;
+    return DiscoveryProcessResult(exitCode: 1, output: output);
+  }
+}
